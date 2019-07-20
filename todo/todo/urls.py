f9f9abeaf48f27cbb118273401b66ca5af553ef6
Vikash Yadav <vikash212000yadav@gmail.com>
2019-07-20 21:16:05 +0530

    status action

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from todo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
